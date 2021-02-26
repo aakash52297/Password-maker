90e4f767a191eb1527f581a4a89b424baf89ec09
Aakash Sharma <aakash.97work@gmail.com>
2021-02-25 23:50:05 -0500

    Updating Javascript

--- script.js
+++ script.js
@@ -7,17 +7,28 @@ var chooseSymbols = document.querySelector("#chooseSymbols");
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers 
-var symbols
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["!", "'", "#", "$", "%", "&", "", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~",];
 
 function randomPassword(characters){
     var character = characters[Math.floor(Math.random() * characters.length)];
     return character; 
 }
 function generatePassword(){
+  var choices = []
     if (lowerCase) {
-        
+      
     }
+    if (upperCase){
+
+    }
+    if (numbers) {
+
+    }
+    if (symbols){
+
+    }
+
 }
 function writePassword() {
   var password = generatePassword();
